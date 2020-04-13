92c39230fd1c1cff2623ae49589cbeea9389dc65
JovanStef <jov.box@outlook.com>
2020-04-13 10:06:42 +0200

    Sorting Pagination and Filtering

--- src/routers/taskRoutes.js
+++ src/routers/taskRoutes.js
@@ -3,10 +3,10 @@ const Task = require('../models/task');
 const auth = require('../middleware/auth');
 const router = new express.Router();
 
-router.post('/tasks',auth, async (req, res) => {
+router.post('/tasks', auth, async (req, res) => {
     const task = new Task({
         ...req.body,
-        user:req.user._id
+        user: req.user._id
     })
     try {
         await task.save();
@@ -22,10 +22,29 @@ router.post('/tasks',auth, async (req, res) => {
 
 });
 
-router.get('/tasks',auth, async (req, res) => {
+// GET /tasks?completed=true
+// GET /tasks?limit=1&skip=1
+// GET /tasks?sortBy=createdAt_asc
+router.get('/tasks', auth, async (req, res) => {
+    const match = {};
+    const sort = {};
+
+    req.query.completed ? match.completed = req.query.completed === 'true' : null;
+    if (req.query.sortBy) {
+        const parts = req.query.sortBy.split('_');
+        sort[parts[0]] = parts[1] === 'asc' ? 1 : -1;
+    }
     try {
         // const tasks = await Task.find({user:req.user._id});
-        await req.user.populate('tasks').execPopulate();
+        await req.user.populate({
+            path: 'tasks',
+            match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip),
+                sort
+            }
+        }).execPopulate();
         res.status(200).send(req.user.tasks);
     } catch (err) {
         res.status(400).send(err.message);
@@ -37,10 +56,10 @@ router.get('/tasks',auth, async (req, res) => {
     // })
 });
 
-router.get('/tasks/:id',auth, async (req, res) => {
+router.get('/tasks/:id', auth, async (req, res) => {
     const _id = req.params.id;
     try {
-        const task = await Task.findOne({_id , user:req.user.id})
+        const task = await Task.findOne({ _id, user: req.user.id })
         task ? res.status(200).send(task) : res.status(404).send({ error: 'No such task' });
     } catch (err) {
         res.status(400).send(err.message);
@@ -55,7 +74,7 @@ router.get('/tasks/:id',auth, async (req, res) => {
     // })
 });
 
-router.patch('/tasks/:id',auth, async (req, res) => {
+router.patch('/tasks/:id', auth, async (req, res) => {
     const keys = Object.keys(req.body);
     const updateKeys = ['name', 'completed'];
     const matchKeys = keys.every((key) => updateKeys.includes(key));
@@ -65,7 +84,7 @@ router.patch('/tasks/:id',auth, async (req, res) => {
     try {
         // const task = await Task.findByIdAndUpdate(req.params.id , req.body , { new:true , runValidators:true});
         // const task = await Task.findById(req.params.id);
-        const task = await Task.findOne({_id:req.params.id , user: req.user._id})
+        const task = await Task.findOne({ _id: req.params.id, user: req.user._id })
 
         task ? res.status(200).send(task) : res.status(404).send({ error: 'No task found' });
         keys.forEach((key) => task[key] = req.body[key]);
@@ -75,10 +94,10 @@ router.patch('/tasks/:id',auth, async (req, res) => {
     }
 });
 
-router.delete('/tasks/:id',auth, async (req, res) => {
+router.delete('/tasks/:id', auth, async (req, res) => {
     try {
         // const task = await Task.findByIdAndDelete(req.params.id);
-        const task = await Task.findOne({_id:req.params.id , user: req.user._id});
+        const task = await Task.findOne({ _id: req.params.id, user: req.user._id });
         task ? res.status(200).send(task) : res.status(404).send({ error: 'No such task' });
     } catch (err) {
         res.status(404).send(err.message);
